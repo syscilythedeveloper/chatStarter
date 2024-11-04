54d9d75358f2cc1d4af7c7ca56996a7a51e0b618
Syscily Brown <sbrown@syscilythedeveloper.com>
2024-11-04 16:35:52 -0500

    convex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,45 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+interface Message {
+  sender: string;
+  content: string;
+}
 
 export default function Home() {
-  return <div>Hello World</div>;
+  const [messages, setMessages] = useState<Message[]>([
+    {
+      sender: "Syscily",
+      content: "Hey Gwourl",
+    },
+    {
+      sender: "Kara",
+      content: "Let's HH",
+    },
+  ]);
+
+  const [input, setInput] = useState("");
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setMessages([...messages, { sender: "Syscily", content: input }]);
+    setInput("");
+  };
+  return (
+    <div>
+      {messages.map((message, index) => (
+        <div key={index}>
+          <strong> {message.sender}</strong> : {message.content}
+        </div>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="message"
+          id="message"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">Send </button>
+      </form>
+    </div>
+  );
 }
